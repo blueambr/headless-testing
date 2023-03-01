6f2c6157e0c2bf3b6750e371d343f2f2ba63d81f
Vlad Gerasimovich <hotepp@icloud.com>
2023-03-01 12:50:45 +0100

    feat: update the init structure

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,5 +2,7 @@ module.exports = {
   extends: ["next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "import/no-extraneous-dependencies": "off",
+    "no-console": ["warn", { allow: ["error", "info", "warn"] }],
   },
 };
